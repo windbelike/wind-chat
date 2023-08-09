2034be89cd9a6b08a1722ef3529b230394d40735
sawyerrick <1103031078@qq.com>
2023-08-09 13:08:41 +0800

    feat: chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,33 @@ export default function Home() {
   }, [])
 
   return (
-    <div> last received data: {data} </div>
+    <>
+      <div className='container min-h-screen flex justify-center'>
+        <div> last received data: {data} </div>
+        <div className='flex justify-center items-center w-full'>
+          <Chat />
+        </div>
+      </div>
+    </>
+  )
+}
+
+function Chat() {
+  return (
+    <div className='w-5/6 flex-col bg-black rounded-md'>
+      <header className='text-white'>
+        chat header
+      </header>
+      <main className='text-white h-96'>
+        haha
+      </main>
+      <div className='p-6 flex text-xl'>
+        <p className='mx-3 text-green-400 font-bold'>{'>'}</p>
+        <input className='text-white bg-transparent outline-none w-full'
+        placeholder='Say something...'
+        autoComplete='off'
+        maxLength={100} />
+      </div>
+    </div>
   )
 }
